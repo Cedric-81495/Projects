9dbe94bcdea4635434103d1f642dae4478fd850b
Jhon Cedric <cedricsusmerano@gmail.com>
2025-08-03 15:02:43 +0800

    Add ScrollToTop component and update App.jsx to enable scroll-to-top on route change

--- Spotify-project/frontend/src/App.jsx
+++ Spotify-project/frontend/src/App.jsx
@@ -5,12 +5,13 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import AppRoutes from './routes/AppRoutes';
 import NowPlayingBar from './components/NowPlayingBar';
-
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
   return (
     <BrowserRouter>
       <SpotifyProvider>
+        <ScrollToTop /> 
         <div className="flex flex-col min-h-screen bg-spotify-black">
           <Navbar />
           <main className="flex-grow pb-20">

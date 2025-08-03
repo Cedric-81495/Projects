6512cd0b10d2f3698ac43532b55bb88b0f3ab972
Jhon Cedric <cedricsusmerano@gmail.com>
2025-08-03 15:58:52 +0800

    Add BackToTop component and update App.jsx to use back to top button

--- Spotify-project/frontend/src/App.jsx
+++ Spotify-project/frontend/src/App.jsx
@@ -6,12 +6,14 @@ import Footer from './components/Footer';
 import AppRoutes from './routes/AppRoutes';
 import NowPlayingBar from './components/NowPlayingBar';
 import ScrollToTop from './components/ScrollToTop';
+import BackToTop from './components/BackToTop';
 
 function App() {
   return (
     <BrowserRouter>
       <SpotifyProvider>
-        <ScrollToTop /> 
+         <ScrollToTop /> 
+         <BackToTop /> 
         <div className="flex flex-col min-h-screen bg-spotify-black">
           <Navbar />
           <main className="flex-grow pb-20">

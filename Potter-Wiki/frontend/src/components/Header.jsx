225b9a3d794596404326e49b386d2a3cbf86bedf
Jhon Cedric <cedricsusmerano@gmail.com>
2025-11-04 15:51:55 +0800

    Updated Header.jsx

--- Potter-Wiki/frontend/src/components/Header.jsx
+++ Potter-Wiki/frontend/src/components/Header.jsx
@@ -9,7 +9,7 @@ export default function Header() {
         <img
           src={PotterWikiHeader}
           alt="Potter Wiki Logo"
-          className="object-contain mx-auto w-[430px] h-[50px] sm:w-[180px] sm:h-[60px] xs:w-[150px] xs:h-[50px]"
+          className="object-contain mx-auto"
           style={{
           width: "clamp(150px, 30vw, 300px)",
           height: "clamp(80px, 10vw, 60px)",

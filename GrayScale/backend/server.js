512c0ef397ca0473964d89995d61e0a388c75b34
Jhon Cedric <cedricsusmerano@gmail.com>
2025-12-31 02:15:33 +0800

    Updated server.js

--- GrayScale/backend/server.js
+++ GrayScale/backend/server.js
@@ -1,10 +1,9 @@
-const dotenv = require("dotenv");
-dotenv.config();
-
-// API Routes
+const path = require("path");
 const express = require("express");
 const cors = require("cors");
+const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+
 const userRoutes = require("./routes/userRoutes");
 const productRoutes = require("./routes/productRoutes");
 const cartRoutes = require("./routes/cartRoutes");
@@ -12,22 +11,29 @@ const checkoutRoutes = require("./routes/checkoutRoutes");
 const orderRoutes = require("./routes/orderRoutes");
 const uploadRoutes = require("./routes/uploadRoutes");
 const subscribeRoute = require("./routes/subscribeRoute");
-const adminOrderRoutes = require("./routes/adminOrderRoutes");
 
-// Admin Routes
 const adminRoutes = require("./routes/adminRoutes");
 const productAdminRoutes = require("./routes/productAdminRoutes");
+const adminOrderRoutes = require("./routes/adminOrderRoutes");
 
+dotenv.config();
+connectDB();
 
 const app = express();
-app.use(express.json());
-app.use(cors());
-
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
 
-// Connect to MongoDB
-connectDB();
+// CORS
+app.use(
+  cors({
+    origin:
+      process.env.NODE_ENV === "production"
+        ? "https://grayscale-kcb9.onrender.com"
+        : "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("Welcome to GrayScale API!");
@@ -42,12 +48,21 @@ app.use("/api/orders", orderRoutes);
 app.use("/api/upload", uploadRoutes);
 app.use("/api", subscribeRoute);
 
-// Admin Routes
+// Admin
 app.use("/api/admin/users", adminRoutes);
 app.use("/api/admin/products", productAdminRoutes);
 app.use("/api/admin/orders", adminOrderRoutes);
 
+// Serve frontend
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });

fd8cbc0e935ab760bc21ae0a2f4de4b3f50b1457
Jhon Cedric <cedricsusmerano@gmail.com>
2026-01-06 12:35:54 +0800

    Updated axiosInstance

--- GrayScale/frontend/src/utils/axiosInstance.js
+++ GrayScale/frontend/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://grayscale-alpha.vercel.app"
+  baseURL: import.meta.env.VITE_BACKEND_URL || "https://mern-grayscale.onrender.com",
 });
 
 axiosInstance.interceptors.request.use((config) => {

7d15b19c7b79bd8f57cc78610982592ef2f9373b
Jhon Cedric <cedricsusmerano@gmail.com>
2026-01-11 04:39:24 +0800

    Updated axiosInstance.js

--- GrayScale/frontend/src/utils/axiosInstance.js
+++ GrayScale/frontend/src/utils/axiosInstance.js
@@ -2,9 +2,10 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: import.meta.env.DEV
-    ? "http://localhost:5000" // Local backend during development
-    : "/",                 // Production: same origin as frontend
+    ? "http://localhost:5000"
+    : import.meta.env.VITE_BACKEND_URL,
 });
+
 
 axiosInstance.interceptors.request.use((config) => {
   const token = localStorage.getItem("userToken");

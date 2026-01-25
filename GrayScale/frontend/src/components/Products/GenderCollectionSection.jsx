5d97ccd2c4b4e0f567d7f10544a0a13b709720fd
Jhon Cedric <cedricsusmerano@gmail.com>
2026-01-25 19:03:07 +0800

    Upadated GenderCollectionSection.jsx

--- GrayScale/frontend/src/components/Products/GenderCollectionSection.jsx
+++ GrayScale/frontend/src/components/Products/GenderCollectionSection.jsx
@@ -18,7 +18,7 @@ const GenderCollectionSection = () => {
                     <h2 className="text-2xl font-bold text-gray-900 mb-3">
                         Women's Collection
                     </h2>
-                    <Link to="/collection/all?gender=Women" className="underline">
+                    <Link to="/collections/all?gender=Women" className="underline">
                        Shop Now
                     </Link>
                 </div>
@@ -34,7 +34,7 @@ const GenderCollectionSection = () => {
                     <h2 className="text-2xl font-bold text-gray-900 mb-3">
                         Men's Collection
                     </h2>
-                    <Link to="/collection/all?gender=Men" className="underline">
+                    <Link to="/collections/all?gender=Men" className="underline">
                        Shop Now
                     </Link>
                 </div>

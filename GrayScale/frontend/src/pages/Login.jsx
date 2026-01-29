b389bff8ea30b092105834e1bd6d3da8ecc662be
Jhon Cedric <cedricsusmerano@gmail.com>
2026-01-29 20:28:28 +0800

    Update resources completed

--- GrayScale/frontend/src/pages/Login.jsx
+++ GrayScale/frontend/src/pages/Login.jsx
@@ -61,7 +61,7 @@ const Login = () => {
         <div className="w-full md:w-1/2 flex flex-col justify-center items-center p-8 md:p-12">
           <form
             onSubmit={handleSubmit}
-            className="w-full max-w-md bg-gray-300 p-8 rounded-lg border shadow-sm"
+            className="w-full max-w-md mt-[-100px] bg-gray-300 p-8 rounded-lg border shadow-sm"
           >
             <div className="flex justify-center mb-6">
               <h2 className="text-xl font-medium">GrayScale</h2>

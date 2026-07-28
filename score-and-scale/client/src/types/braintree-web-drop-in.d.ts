7c38a302eadda9782f710f986c18505fb2f8a613
Cedric Susmerano <cedricsusmerano@gmail.com>
2026-07-28 23:05:03 +0800

    Remove all existing files

--- score-and-scale/client/src/types/braintree-web-drop-in.d.ts
+++
@@ -1,8 +0,0 @@
-// client/src/types/braintree-web-drop-in.d.ts
-declare module "braintree-web-drop-in" {
-  export interface Dropin {
-    requestPaymentMethod: () => Promise<{ nonce: string; type: string }>;
-    teardown: () => Promise<void>;
-    on: (event: string, handler: (...args: any[]) => void) => void;
-  }
-}
